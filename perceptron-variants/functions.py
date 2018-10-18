377f3ee2cabefbb6045f019da7f94328290b496e
Matthew Liechty <matthewliechty@macbooks-MacBook-Pro.local>
2018-10-17 21:33:53 -0600

    updating ignore

--- perceptron-variants/functions.py
+++
@@ -1,4 +0,0 @@
-def sign(num):
-    if num >= 0:
-        return 1.0
-    return -1.0
--- perceptron-variants/perceptron_results.sh
+++
@@ -1,5 +0,0 @@
-python3 run_simple.py
-python3 run_decaying.py
-python3 run_margin.py
-python3 run_averaged.py
-python3 run_aggressive.py
